25a5ad6df5efb54e11e8231bfd7ea11848fec5ad
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-04-11 08:55:43 -0500

    Submit Class inheritance assignment

---
+++ Inheritance.py
@@ -0,0 +1,31 @@
+class User:
+    #Define the attributes of the class
+    name = "No Name Provided"
+    email = " "
+    password = "1234abcd"
+    account = 0
+    #initialize class
+    def __init__ (self, name, email, password, account):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.account = account
+    
+#add child classes (customer and vendor)
+class Customer(User):
+    mailing_address = " "
+    mailing_list = True
+
+class Vendor(User):
+    vendor_company = "General"
+    vendor_phone = "123-456-7890"
+
+    #Define the methods of the class
+    def login(self):
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your password: ")
+        if (entry_email == self.email and entry_password==self.password):
+            print("Welcome back, {}.".format(self.name))
+        else:
+            print("You are not authorized for this page.")
+
