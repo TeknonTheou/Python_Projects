e9fe8ce6ff007080a27d0e4ca64973cc2b6f7a57
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-04-11 15:54:53 -0500

    Update Polymorphism.py

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,64 +1,57 @@
 class User:
     #Define the attributes of the class
-    name = "No Name Provided"
-    email = " "
-    password = "1234abcd"
-    account = 0
-    #initialize class
-    def __init__ (self, name, email, password, account):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.account = account
+    name = "Billy"
+    email = "teknontheou@gmail.com"
+    password = "abcd1234"
 
- #Define the methods of the class
-    def login(self):
+    #Define the methods of the class
+    def getLoginInfo(self):
         entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
+        entry_email = input("Enter your email:  ")
         entry_password = input("Enter your password: ")
         if (entry_email == self.email and entry_password==self.password):
             print("Welcome back, {}.".format(entry_name))
         else:
             print("You are not authorized for this page.")
 
-    
-#add child classes (customer and vendor)
+#add child class Customer
 class Customer(User):
     mailing_address = " "
     mailing_list = True
     pin_number = "5678"
 
     #Define the methods of the child class
-    def login(self):
+    def getLoginInfo(self):
         entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
-        entry_pin = input("Enter your password: ")
+        entry_email = input("Enter your email:  ")
+        entry_pin = input("Enter your pin: ")
         if (entry_email == self.email and entry_pin==self.pin_number):
             print("Welcome back, {}.".format(entry_name))
         else:
-            print("Invalid email or pin.")
+            print("User email or pin is incorrect.")
 
+#add child class Vendor
 class Vendor(User):
     vendor_company = "General"
     vendor_phone = "123-456-7890"
+    vendor_id = "TeknonTheou"
     pin_number = "6789"
 
     #Define the methods of the child class
-    def login(self):
-        entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
-        entry_pin = input("Enter your password: ")
-        if (entry_email == self.email and entry_pin==self.pin_number):
-            print("Welcome back, {}.".format(entry_name))
+    def getLoginInfo(self):
+        entry_id = input("Enter your vendor id: ")
+        entry_pin = input("Enter your pin: ")
+        if (entry_id == self.vendor_id and entry_pin==self.pin_number):
+            print("Welcome back, {}.".format(self.name))
         else:
-            print("Invalid email or pin.")
+            print("User id or pin is incorrect.")
 
-# Unvoke method inside each class.
-manager = User()
-magager.login
+
+employee = User()
+employee.getLoginInfo()
 
 customer = Customer()
-customer.login
-
+customer.getLoginInfo()
+    
 vendor = Vendor()
-vendor.login
+vendor.getLoginInfo()
