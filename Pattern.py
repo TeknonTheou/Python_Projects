ca5d944511fe2e715b596b84792a8b6d0df6309d
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-07-14 15:54:38 -0500

    Update Python Projects

---
+++ Pattern.py
@@ -0,0 +1,41 @@
+import turtle
+
+turtle.speed(0)
+
+turtle.color('purple')
+for p in range(24):
+    turtle.rt(15)
+    for q in range(36):
+        turtle.lt(10)
+        turtle.fd(35)
+
+turtle.color('blue')       
+for r in range(24):
+    turtle.rt(15)
+    for s in range(36):
+        turtle.lt(10)
+        turtle.fd(30)
+        
+turtle.color('green')
+for t in range(24):
+    turtle.rt(15)
+    for u in range(36):
+        turtle.lt(10)
+        turtle.fd(25)
+
+turtle.color('orange')
+for t in range(24):
+    turtle.rt(15)
+    for u in range(36):
+        turtle.lt(10)
+        turtle.fd(20)
+
+turtle.color('red')
+for t in range(24):
+    turtle.rt(15)
+    for u in range(36):
+        turtle.lt(10)
+        turtle.fd(15)
+        
+        
+
